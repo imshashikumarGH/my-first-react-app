01e4086af195954f1e59f14ee0f9ca31f3178f9b
imshashikumarGH <imshashikumar01@gmail.com>
2023-03-02 19:34:00 +0530

    AB03 Setting backgrounds inactive while error message is in UI

--- src/common/ErrorModule.js
+++ src/common/ErrorModule.js
@@ -5,17 +5,19 @@ import classes from "./ErrorModule.module.css";
 
 const ErrorModule = (props) => {
   return (
-    <Card className={classes.modal}>
-      <header className={classes.header}>
-        <h2>{props.title}</h2>
-      </header>
-      <div className={classes.content}>
-        <p>{props.message}</p>
-      </div>
-      <footer className={classes.actions}>
-        <Button>Okay</Button>
-      </footer>
-    </Card>
+    <div className={classes.backdrop}>
+      <Card className={classes.modal}>
+        <header className={classes.header}>
+          <h2>{props.title}</h2>
+        </header>
+        <div className={classes.content}>
+          <p>{props.message}</p>
+        </div>
+        <footer className={classes.actions}>
+          <Button>Okay</Button>
+        </footer>
+      </Card>
+    </div>
   );
 };
 export default ErrorModule;
