0d3a4743277ee70609d4763c0753a4d52db5651e
imshashikumarGH <imshashikumar01@gmail.com>
2023-03-02 15:44:47 +0530

    AB02 add component to render userList in UI

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import AddUser from "./user/AddUser";
+import UserList from "./user/UserList";
 
 function App() {
   return (
     <div>
       <AddUser></AddUser>
+      <UserList users={[]}/>
     </div>
   );
 }
