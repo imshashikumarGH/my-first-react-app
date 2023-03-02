be103a0733ee036342f9f945538e480b85bbf4b4
imshashikumarGH <imshashikumar01@gmail.com>
2023-03-02 13:18:11 +0530

    AB01 CSS module :  Adding Cards and Cards module css to add both css file

--- src/user/AddUser.js
+++ src/user/AddUser.js
@@ -1,18 +1,23 @@
 import React from "react";
+import Card from "../common/Card";
+import classes from './AddUser.module.css';
+
 
 const AddUser = (props) => {
-    const AddUserHandler = (event) =>{
-        event.preventDefault();
-        }
-    
+  const AddUserHandler = (event) => {
+    event.preventDefault();
+  };
+
   return (
-    <form onSubmit={AddUserHandler}>
-      <label html="username">Username</label>
-      <input id="username" type="text" />
-      <label html="age">Age (Years)</label>
-      <input id="age" type="number" />
-      <button type="submit">Add User</button>
-    </form>
+    <Card indexClassName = {classes.input}>
+      <form onSubmit={AddUserHandler}>
+        <label html="username">Username</label>
+        <input id="username" type="text" />
+        <label html="age">Age (Years)</label>
+        <input id="age" type="number" />
+        <button type="submit">Add User</button>
+      </form>
+    </Card>
   );
 };
 
