c96f83db82b4b3c3f1b3579d48d1bc3774805526
imshashikumarGH <imshashikumar01@gmail.com>
2023-03-02 17:20:04 +0530

    AB02 Function to add user at nearest commom commponent

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
-import React from "react";
+import React, {useState} from "react";
 import AddUser from "./user/AddUser";
 import UserList from "./user/UserList";
 
 function App() {
+  const [userList, setUserList] = useState([]);
+
+  const addUserbyInput = (uName, uAge) => {
+    setUserList((prevUserList) => {
+      const user = {
+        name: uName,
+        age: uAge,
+      };
+      return [user, ...prevUserList];
+    });
+  };
   return (
     <div>
-      <AddUser></AddUser>
-      <UserList users={[]}/>
+      <AddUser onAddUser={addUserbyInput}></AddUser>
+      <UserList users={userList} />
     </div>
   );
 }
