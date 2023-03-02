5883fd1750995dd8f61c8c54be78dce2ffa90fc5
imshashikumarGH <imshashikumar01@gmail.com>
2023-03-02 15:13:10 +0530

    AB02 Using useState for reading value fron Form

--- src/user/AddUser.js
+++ src/user/AddUser.js
@@ -1,21 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import Card from "../common/Card";
 import classes from './AddUser.module.css';
 import Button from "../common/Button";
 
 
 const AddUser = (props) => {
+  const [enteredUserName, setEnteredUserName]  = useState();
+  const [enteredAge, setEnteredAge]  = useState();
+
   const AddUserHandler = (event) => {
     event.preventDefault();
   };
+
+  const UserNameChangeHandler = (event) =>{
+    setEnteredUserName(event.target.value);
+  }
+
+  const ageChangeHandler = (event) =>{
+    setEnteredAge(event.target.value);
+  }
 
   return (
     <Card indexClassName = {classes.input}>
       <form onSubmit={AddUserHandler}>
         <label html="username">Username</label>
-        <input id="username" type="text" />
+        <input id="username" type="text" onClick={UserNameChangeHandler}/>
         <label html="age">Age (Years)</label>
-        <input id="age" type="number" />
+        <input id="age" type="number" onClick={ageChangeHandler}/>
         <Button type="submit">Add User</Button>
       </form>
     </Card>
