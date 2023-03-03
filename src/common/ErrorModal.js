14022dfa5495a09986e830bcc4a53d294696a5ce
imshashikumarGH <imshashikumar01@gmail.com>
2023-03-03 11:18:23 +0530

    AB04 Creating two modal for backdrop and error Message

--- src/common/ErrorModal.js
+++ src/common/ErrorModal.js
@@ -4,8 +4,13 @@ import Card from "./Card";
 import classes from "./ErrorModal.module.css";
 
 const ErrorModule = (props) => {
-  return (
-    <div className={classes.backdrop} onClick = {props.onConfirm}>
+
+  const backDrop = () => {
+    return <div className={classes.backdrop} onClick={props.onConfirm}></div>;
+  };
+  
+  const errorModal = () => {
+    return (
       <Card className={classes.modal}>
         <header className={classes.header}>
           <h2>{props.title}</h2>
@@ -14,10 +19,12 @@ const ErrorModule = (props) => {
           <p>{props.message}</p>
         </div>
         <footer className={classes.actions}>
-          <Button onClick = {props.onConfirm}>Okay</Button>
+          <Button onClick={props.onConfirm}>Okay</Button>
         </footer>
       </Card>
-    </div>
-  );
+    );
+  };
+
+  return <React.Fragment></React.Fragment>;
 };
 export default ErrorModule;
